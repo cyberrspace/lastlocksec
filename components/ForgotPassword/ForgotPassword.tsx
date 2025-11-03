fc690ff8a4c9c5b41e5992c9c9dcb2165c535bc2
cyberrspace <xhamooel95@gmail.com>
2025-11-03 18:21:02 +0100

    forgot password

--- components/ForgotPassword/ForgotPassword.tsx
+++ components/ForgotPassword/ForgotPassword.tsx
@@ -4,14 +4,14 @@ import BackButton from "../common/BackButton";
 
 export default function ForgotPassword() {
   return (
-    <main className="w-full min-h-screen flex flex-col justify-center items-center px-4 sm:px-6 md:px-8 py-10">
+    <main className="w-full h-auto sm:h-screen flex flex-col justify-center items-center px-4 sm:px-6 md:px-8">
       {/* Back Button */}
-      <div className="w-full max-w-[512px] mb-4">
+      <div className="w-full max-w-[512px] mb-3 sm:mb-4">
         <BackButton />
       </div>
 
       {/* Form Card */}
-      <section className="w-full max-w-[512px] bg-[#2C2C2C] rounded-[12px] py-8 sm:py-10 md:py-12 px-6 sm:px-8 flex flex-col items-center text-center">
+      <section className="w-full max-w-[512px] bg-[#2C2C2C] rounded-[12px] py-6 sm:py-15 md:py-12 px-6 sm:px-8 flex flex-col items-center text-center">
         {/* Title */}
         <h1 className="text-[22px] sm:text-[26px] md:text-[30px] lg:text-[34px] font-bold text-[#F5F5F5] leading-tight">
           Forgot Password
@@ -19,7 +19,7 @@ export default function ForgotPassword() {
 
         {/* Subtitle */}
         <p className="mt-2 sm:mt-3 md:mt-4 text-[13px] sm:text-[14px] md:text-[15px] text-[#D1D1D1] leading-[22px]">
-          Enter your email address to reset your <br className="sm:block" />
+          Enter your email address to reset your <br className="sm:block"/>
           password
         </p>
       </section>
